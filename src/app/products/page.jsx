a808e6cf368583808ccc77d47c3d3d178436e9a5
Benson Njuguna <njugunab655@gmail.com>
2023-07-15 09:29:44 +0300

    Change with for small screens

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -34,16 +34,20 @@ const Products = async () => {
                   <Link
                     key={id}
                     href={`/products/${id}`}
-                    className="w-64 border m-2 bg-white p-2 rounded-xl flex flex-col relative shadow-lg shadow-blue-500/30 hover:shadow-indigo-500/50"
+                    className="w-full sm:w-64 border m-2 bg-white p-2 rounded-xl flex flex-col relative shadow-lg shadow-blue-500/30 hover:shadow-indigo-500/50"
                   >
                     <Image
                       src={image}
                       alt={title}
                       height="200"
                       width="200"
-                      className="h-1/2 w-full grow"
+                      className="w-full grow"
                     />
                     <div className="flex my-2">
+                      <h4 className="font-bold mr-auto">
+                        $
+                        {price}
+                      </h4>
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                         <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clipRule="evenodd" />
                       </svg>
@@ -55,17 +59,10 @@ const Products = async () => {
                       </span>
                     </div>
                     <h4 className="">{title}</h4>
-                    <h4 className="font-bold mt-2">
-                      $
-                      {price}
-                    </h4>
-                    <Button name ="Add to Cart" />
-                    <button
-                      type="button"
-                      className="border rounded-lg p-2 w-full mt-4 hover:text-orange-400 hover:font-bold hover:bg-blue-950 transition-all"
-                    >
-                      Details
-                    </button>
+                    <div className='flex gap-4'>
+                      <Button name ="Add to Cart" />
+                      <Button name="Details" />
+                    </div>
                   </Link>
                 ))}
               </ul>
