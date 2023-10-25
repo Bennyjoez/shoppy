f32196b552e453b6b8edcf5be83b60545c032059
Benson <njugunab655@gmail.com>
2023-10-25 19:08:35 +0300

    Fix errors

--- src/lib/getComments.js
+++ src/lib/getComments.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export default async function getComments() {
   const res = await axios.get("https://jsonplaceholder.typicode.com/posts/1/comments");
 
-  return res;
+  return res.data;
 }
